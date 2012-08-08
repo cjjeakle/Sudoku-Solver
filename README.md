cdee74816a0915584ad5af03cb7fef85881a8370
cjjeakle <cjjeakle@gmail.com>
2012-08-08 03:00:10 -0400

    Changes: ->fixed spelling error using sodoku instead of sudoku (will need to rename the repo somehow) ->firstRun has been renamed to clearMatches, it no longer needs to be commented out for the program to work ->fixed mathematical error finding a square's "sub-board" coordinates (interger divide by 3 instead of 9) ->added color! ->improved notes in code ->moved improvments to be made from the commit message into the readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Sodoku-Solver
+Sudoku-Solver
 =============
 
-A c++ project intended to solve sodoku puzzles
+A c++ project intended to solve sudoku puzzles
+
+In its current state the program solves simples sudoku puzzles
+Test board is a simple test sudoku board to paste when prompted for input
+
+Changes to make:
+-> find a way to identify two squares in a row or col of a sub-board that have an identical two possibilities, eliminate using these
+-> clean up code, possibly divide into multiple files, and possibly allow for file and console input (is there a real need?)
+-> add error checking of user input (could prove quite complex)
+
+->EVENTUALLY: port to javascript, make a mobile compatible website
---
+++ sudoku-solver.cpp
@@ -0,0 +1,390 @@
+//Sudoku Solver
+//written by: Chris Jeakle (cjjeakle)
+
+#include <iostream>
+
+//credit goes to shuttle87 on this suggestion for color coding output
+//http://stackoverflow.com/questions/9158150/colored-output-in-c
+//THESE COLOR CODES ARE FOR MAC OS AND UBUNTU TERMINAL ONLY
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
+using namespace std;
+
+/*
+how to imagine the data:
+a 9x9 array of "squares", or a 3x3 array or sub-boards
+
+the board is an array arranged as such:
+ 0,0 | 0,1 | 0,2...
+ --- | --- | ---
+ 1,0 | 1,1 | 1,2...
+ --- | --- | ---
+ 2,2 | 2,1 | 2,2...
+  .     .     .
+  .     .     .
+  .     .     .
+  
+there are 9 sub-boards to consider, too (the nine 3x3 squares that can't contain repeats)
+These are arranged as:
+				    0 1 2
+				    - - -
+				  0|1 2 3
+				  1|4 5 6
+				  2|7 8 9
+a square's sub-board coordinates can be determined by interger dividing row and col coordinates by 3.
+  
+  
+inside each of the 81 "squares" are 9 variables representing the 9 possible answers to that square:
+ 
+			  one  |  two   | three
+			  ---  |  ---   |  ---
+			  four |  five  |  six
+			  ---  |  ---   |  ---
+			 seven | eight  |  nine
+			 	  ---
+			 	  TEN
+ 
+	each is assigned a 0, 1, or 2
+	0 = eliminated possibility
+	1 = part of the origional board or garanteed true
+	2 = possibility (more than one answer possible for this location)
+	
+	TEN (array reference 9) is unique, it stores the solution for a square 
+	or a 0 if none is found yet
+*/
+
+
+
+//Requires: a fully populated board, with any int value in the 9*9*10 array locations
+//Modifies: N/A
+//Effects: prints the sudoku board with all garanteed squares showing their 
+//	solution and all others showing a "?" 
+void printBoard (int board[9][9][10]);
+
+//Requires: basis be a garanteed correct answer for row and col, board is populated
+//Modifies: board
+//Effects: takes a row and col, finds its equilivent sub-board, and removes
+//	"basis" as a possibility for all squares in this sub-board except 
+//	that with the row and col given
+void eliminateSubBoard (int board[9][9][10], int basis, int row, int col);
+
+//Requires: basis be a garanteed correct answer for row and col, board is populated
+//Modifies: board
+//Effects: takes a row and removes "basis" as a possibility for all squares except
+//	the square in "col"
+void eliminateRow (int board[9][9][10], int basis, int row, int col);
+
+//Requires: basis be a garanteed correct answer for row and col, board is populated
+//Modifies: board
+//Effects: takes a col and removes "basis" as a possibility for all squares except
+//	the square in "row"
+void eliminateCol (int board[9][9][10], int basis, int row, int col);
+
+//Requires: a board that has just been populated by the user
+//Modifies: board
+//Effects: takes a newly populated board and eliminates the givens as possibilities
+//	for conflicting squares
+void clearMatches (int board[9][9][10]);
+
+//Requires: a valid board
+//Modifies: board
+//Effects: finds all squares with one possible number as a solution, assigns that
+//	single possibility as a solution to the square.
+//	possibilities in the sub-board, row and col that match the solved num are removed
+void solveSingletons (int board[9][9][10]);
+
+//Requires: a valid board
+//Modifies: N/A
+//Effects: returns false if any squares still need solutions, returns true otherwise
+bool checkComplete (int board[9][9][10]);
+
+int main()
+{
+	int board[9][9][10];
+	
+	
+	//initilize the board
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			board [i][j][9] = 0;
+			//at this point every number is a possibility
+			for (int n = 0; n < 9; n++)
+			{
+				board [i][j][n] = 2;
+			}
+		}
+	}
+	
+	//populate givens
+	for (int i = 0; i < 9; i++)
+	{
+		cout << "input the data for row " << i+1 <<" , separate with spaces and use 0 to denote blanks" <<endl;
+		int x = 0;
+		
+		for (int j = 0; j < 9; j++)
+		{
+			//we assign a 1 to any location values that are garanteed true
+			cin >> x;
+			if (x != 0)
+			{
+				board [i][j][(x - 1)] = 1;
+				board [i][j][9] = x;
+				
+				for (int n = 0; n < 9; n++)
+				{	
+					if (x != n)
+					{
+						board [i][j][n] = 0;
+					}
+				}
+			}
+		}
+	}
+	clearMatches (board);
+	
+	bool complete = false;
+	int checker = 0;
+	while (!complete && checker < 100000)
+	{
+		//iterate through solving step 5 times and check if complete, repeat 
+		for (int i = 0; i < 5; i++)
+		{
+			solveSingletons (board);
+			checker++;
+		}
+		complete = checkComplete(board);	
+	}
+	
+	cout << checker <<endl;
+	//print what should be a solved board
+	printBoard (board);
+	return 0;
+}
+
+
+void printBoard (int board[9][9][10])
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			//if there is a solution for this square, print that
+			if (board[i][j][9] != 0)
+			{	
+				cout << GREEN << " " << board[i][j][9] <<RESET <<" ";
+			}
+			//otherwise, print a question mark
+			else
+			{
+				cout << " ? ";
+			}
+			//separate squares with a "|"
+			if (j < 8)
+			{
+				if (j % 3 == 2)
+				{
+					cout << BLUE;
+				}
+				cout << " | ";
+				if (j % 3 == 2)
+				{
+					cout << RESET;
+				}
+			}
+		}
+		cout << endl;
+		
+		//formatting
+		for (int j = 0; j < 9; j++)
+		{
+			if (i < 8)
+			{
+				if (i % 3 == 2)
+				{
+					cout << BLUE;
+				}
+				cout << "---";
+				if (i % 3 == 2)
+				{
+					cout << RESET;
+				}
+				if (j < 8)
+				{ 
+					if (j % 3 == 2)
+					{
+						cout << BLUE;
+					}
+					
+					cout << " | ";
+					
+					if (j % 3 == 2)
+					{
+						cout << RESET;
+					}
+				}
+			}
+		}
+		cout << endl;
+	}
+	/*(outputs the current possibilities being considered)
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << i << " " << j <<endl;
+			for (int n = 0; n < 10; n++)
+			{
+				cout << " location:" << n <<" value: " <<board[i][j][n] <<endl;
+			}
+			cout << endl <<endl <<endl;
+		}
+	}
+	*/
+}
+	
+
+void eliminateSubBoard (int board[9][9][10], int basis, int row, int col)
+{
+	int subBoardRow = row/3;
+	int subBoardCol = col/3;
+	
+	//navigate the rows
+	for (int i = 0; i < 9; i++)
+	{
+		//navigate the cols
+		for (int j = 0; j < 9; j++)
+		{
+			if ((board [i][j][9] == 0) && (i/3 == subBoardRow)\
+			&& (j/3 == subBoardCol) && (i != row) && (j != col)) 
+			{
+				board[i][j][(basis - 1)] = 0;
+			}
+		}
+	}
+}
+
+
+void eliminateRow (int board[9][9][10], int basis, int row, int col)
+{
+	//navigate across the row
+	for (int j = 0; j < 9; j++)
+	{
+		if ((board [row][j][9] == 0) && (j != col))
+		{
+			board[row][j][(basis - 1)] = 0;
+		}
+	}
+}
+
+
+void eliminateCol (int board[9][9][10], int basis, int row, int col)
+{
+	//navigate down the col
+	for (int i = 0; i < 9; i++)
+	{
+		if ((board [i][col][9] == 0) && (i != row)) 
+		{
+			board[i][col][(basis - 1)] = 0;
+		}
+	}
+}
+
+void clearMatches (int board[9][9][10])
+{
+	//rows
+	for (int i = 0; i < 9; i++)
+	{	
+		//cols
+		for (int j = 0; j < 9; j++)
+		{
+			//delete any conflicts with the givens
+			if (board[i][j][9] != 0)
+			{
+				eliminateSubBoard (board, board [i][j][9], i, j);
+				eliminateRow (board, board [i][j][9], i, j);
+				eliminateCol (board, board [i][j][9], i, j);
+			}
+		}
+	}
+}
+
+void solveSingletons (int board[9][9][10])
+{
+	//rows
+	for (int i = 0; i< 9; i++)
+	{	
+		//cols
+		for (int j = 0; j < 9; j++)
+		{
+			//if no solution for this square, iterate through possibilities
+			if (board[i][j][9] == 0)
+			{
+				int numPossible = 0;
+				int possibleAns = 0;
+				for (int n = 0; n < 9; n++)
+				{
+					if (board[i][j][n] == 2)
+					{
+						numPossible++;
+						//n+1 translates the array location
+						//into the number it represents
+						possibleAns = (n + 1);
+					}
+				}
+				//if there is only one possibility, that is the solution
+				if (numPossible == 1)
+				{
+					board [i][j][possibleAns] = 1;
+					board [i][j][9] = possibleAns;
+					eliminateSubBoard (board, possibleAns, i, j);
+					eliminateRow (board, possibleAns, i, j);
+					eliminateCol (board, possibleAns, i, j);
+				}
+			}
+		}
+	}
+}
+
+
+bool checkComplete (int board[9][9][10])
+{
+	//rows
+	for (int i = 0; i < 9; i++)
+	{	
+		//cols
+		for (int j = 0; j < 9; j++)
+		{
+			//if a square lacks a solution, the board is incomplete
+			if (board[i][j][9] == 0)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
